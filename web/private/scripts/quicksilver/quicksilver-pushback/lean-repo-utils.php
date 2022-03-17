212facb9abc9cfbfed3017c9629a668de14fbc84
Kevin Porras <kevin.porras@pantheon.io>
2022-03-16 18:57:21 -0600

    Remove -q option for commit.

--- web/private/scripts/quicksilver/quicksilver-pushback/lean-repo-utils.php
+++ web/private/scripts/quicksilver/quicksilver-pushback/lean-repo-utils.php
@@ -220,11 +220,10 @@ function push_back($fullRepository, $workDir, $upstreamRepoWithCredentials, $bui
 
     // TODO: Copy author, message and perhaps other attributes from the commit at the head of the full repository
     print "Git commit command\n";
-    print "git --git-dir=$canonicalRepository/.git -C $fullRepository commit -q --no-edit --message=$comment --author=$author --date=$commit_date";
-    exec("git --git-dir=$canonicalRepository/.git -C $fullRepository commit -q --no-edit --message=$comment --author=$author --date=$commit_date", $output, $commitStatus);
+    print "git --git-dir=$canonicalRepository/.git -C $fullRepository commit --no-edit --message=$comment --author=$author --date=$commit_date";
+    passthru("git --git-dir=$canonicalRepository/.git -C $fullRepository commit --no-edit --message=$comment --author=$author --date=$commit_date", $commitStatus);
     print "Output:\n";
     print $commitStatus;
-    var_dump($output);
 
     // Get our .gitignore back
     passthru("git -C $fullRepository checkout -- .gitignore");
